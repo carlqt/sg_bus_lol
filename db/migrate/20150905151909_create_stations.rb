4cdd83a91a8175f8cfb52f7924ac0a8a8e2a8104
Carl Tablante <carl.q.tablante@gmail.com>
2015-09-05 23:19:31 +0800

    migration for bus_station model

---
+++ db/migrate/20150905151909_create_stations.rb
@@ -0,0 +1,13 @@
+class CreateStations < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.integer :bus_code_id
+      t.integer :station_code
+      t.string :road
+      t.string :description
+      t.string :summary
+
+      t.timestamps null: false
+    end
+  end
+end
