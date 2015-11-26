0ae7388e8bf233dd0f11d352229cc4ce1f8f0049
Carl Tablante <carlwilliam.tablante@gmail.com>
2015-11-26 15:05:00 +0800

    added color coding to bus numbers

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def color_code(time)
+    return if time == "not available"
+    if time >= 10
+      "alert"
+    elsif (5..9).include?(time)
+      "warning"
+    else
+      "success"
+    end
+  end
 end
