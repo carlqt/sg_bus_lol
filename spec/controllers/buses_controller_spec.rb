6591254651a09418b4f7f373b213d33e84360d8f
Carl Tablante <carlwilliam.tablante@gmail.com>
2015-12-13 08:40:12 +0800

    Added a minimal test spec

--- spec/controllers/buses_controller_spec.rb
+++ spec/controllers/buses_controller_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe BusesController, type: :controller do
 
   describe "GET #show" do
+    before do
+      allow_any_instance_of(BusDecorator).to receive(:routes_left_from).with(nil) { nil }
+    end
+
     it "returns http success" do
-      get :show
+      get :show, id: 1
       expect(response).to have_http_status(:success)
     end
   end
--- spec/features/site_spec.rb
+++ spec/features/site_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 feature "Application" do
   it "loads the pages" do
+    visit '/'
+    expect(page).to have_content "SG Bus LoL"
   end
 end
